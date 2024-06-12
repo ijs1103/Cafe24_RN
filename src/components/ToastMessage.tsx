a3124325d4a491af4bb5238bea6609d91c7e24c8
ijs1103 <ajaijs1103@naver.com>
2024-06-12 17:18:28 +0900

    feat: ToastMessage 컴포넌트

---
+++ src/components/ToastMessage.tsx
@@ -0,0 +1,61 @@
+import React, { useRef, forwardRef, useImperativeHandle } from 'react';
+import { Animated, View, Text, Easing } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const ToastMessage = forwardRef((props, ref) => {
+	const translateYAnimation = useRef(new Animated.Value(100)).current;
+
+	const showToastMessage = () => {
+		Animated.sequence([
+			Animated.timing(translateYAnimation, {
+				toValue: 0,
+				duration: 300,
+				easing: Easing.out(Easing.circle),
+				useNativeDriver: true,
+			}),
+			Animated.delay(2000),
+			Animated.timing(translateYAnimation, {
+				toValue: 100,
+				duration: 500,
+				easing: Easing.in(Easing.circle),
+				useNativeDriver: true,
+			}),
+		]).start();
+	};
+
+	useImperativeHandle(ref, () => ({
+		showToastMessage
+	}));
+
+	return (
+		<Animated.View
+			style={{
+				position: 'absolute',
+				bottom: 0,
+				width: '100%',
+				transform: [
+					{
+						translateY: translateYAnimation,
+					},
+				],
+			}}
+		>
+			<View
+				style={{
+					backgroundColor: '#222',
+					flexDirection: 'row',
+					paddingVertical: 14,
+					paddingHorizontal: 20,
+					margin: 14,
+					borderRadius: 4,
+					alignItems: 'center',
+				}}
+			>
+				<Icon name="checkmark-circle" color="white" size={24} />
+				<Text style={{ color: 'white', fontSize: 15, marginLeft: 10 }}>
+					오류가 발견됐습니다!
+				</Text>
+			</View>
+		</Animated.View>
+	);
+});
